6e524d087f22dc66d9c8c16a2c1ae87e68fa0378
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-03-01 15:56:12 +0900

    add about

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "アバウトページ"
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: "description", name: "description", content: "description" }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss"></style>

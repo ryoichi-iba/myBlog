3564d8866e22bd864b6639af5923516f37b6d0e9
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-03-01 15:04:54 +0900

    add index.vue

---
+++ pages/blog/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <p>{{ date }}</p>
+    <div v-html="contents"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://suzukazu-blog.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { "X-API-KEY": process.env.MICRO_CMS_API }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <h1>ブログ一覧</h1>
+    <ul>
+      <li v-for="content in contents" :key="content.id">
+        <nuxt-link :to="`/blog/${content.id}`">
+          {{ content.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </main>
+</template>
+<script>
+import axios from "axios";
+export default {
+  // mounted() {
+  //   console.log(process.env.MICRO_CMS_API);
+  // },
+
+  async asyncData() {
+    const { data } = await axios.get(
+      // 5-2でメモしたURLを記載しましょう。
+      "https://suzukazu-blog.microcms.io/api/v1/blog",
+      {
+        // 5-2でメモしたAPIキーを記載しましょう。
+        headers: { "X-API-KEY": process.env.MICRO_CMS_API }
+      }
+    );
+    return data;
+  }
+};
+</script>
+<style></style>
